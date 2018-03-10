2f754c244cf75241949534b789856ee53294edca
dmytroyurchyk <33642789+dmytroyurchyk@users.noreply.github.com>
2018-03-10 12:54:26 -0500

    Update GoodMorning.java

--- src/main/GoodMorning.java
+++ src/main/GoodMorning.java
@@ -3,6 +3,7 @@ package main;
 public class GoodMorning {
 	public static void main(String[] args) {
 		System.out.println("Good Evening Class!");
+		System.out.println("Shel na hyi");
 		System.out.println("Changes");
 	}
 }
